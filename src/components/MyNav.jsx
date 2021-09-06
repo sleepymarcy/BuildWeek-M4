f35d48e33fb7e370e3162ad9ecfa85b15d5feed3
Martyna <martyna.sowinska@hotmail.com>
2021-09-06 11:39:37 +0200

    Fixed

--- src/components/MyNav.jsx
+++ src/components/MyNav.jsx
@@ -1,4 +1,4 @@
-import { Navbar, NavDropdown, Button, Form, FormControl } from 'react-bootstrap'
+import { Navbar, Nav, NavDropdown, Button, Form, FormControl } from 'react-bootstrap'
 
 const MyNav = () => {
     return (
@@ -6,9 +6,9 @@ const MyNav = () => {
             <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
             <Navbar.Toggle aria-controls="basic-navbar-nav" />
             <Navbar.Collapse id="basic-navbar-nav">
-                <MyNav className="mr-auto">
-                    <MyNav.Link href="#home">Home</MyNav.Link>
-                    <MyNav.Link href="#link">Link</MyNav.Link>
+                <Nav className="mr-auto">
+                    <Nav.Link href="#home">Home</Nav.Link>
+                    <Nav.Link href="#link">Link</Nav.Link>
                     <NavDropdown title="Dropdown" id="basic-nav-dropdown">
                         <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
                         <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
@@ -16,7 +16,7 @@ const MyNav = () => {
                         <NavDropdown.Divider />
                         <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
                     </NavDropdown>
-                </MyNav>
+                </Nav>
                 <Form inline>
                     <FormControl type="text" placeholder="Search" className="mr-sm-2" />
                     <Button variant="outline-success">Search</Button>
