12a453435e01cedfb3376ae3e29740a20e28a414
Martyna <martyna.sowinska@hotmail.com>
2021-09-06 19:47:05 +0200

    Fixed position

--- src/components/MyNav.jsx
+++ src/components/MyNav.jsx
@@ -8,6 +8,7 @@ const MyNav = () => {
     return (
 
         <Navbar bg="light" expand="lg">
+            <div className='nav-wrapper'>
                 <Navbar.Brand href="#home"><Linked /></Navbar.Brand>
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse id="basic-navbar-nav">
@@ -20,17 +21,18 @@ const MyNav = () => {
                         </InputGroup>
                     </Form>
 
-                    <Nav className="mr-auto">
+                    <Nav className='nav'>
                         <Nav.Link href="#home" className='link-dropdown' ><Hicon /><span className='text-dropdown'>Home</span></Nav.Link>
-                        <Nav.Link href="#link" className='link-dropdown'><Nicon /><span className='text-dropdown'>My Network</span></Nav.Link>
-                        <Nav.Link href="#link" className='link-dropdown'><Jicon /><span className='text-dropdown'>Jobs</span></Nav.Link>
-                        <Nav.Link href="#link" className='link-dropdown'><Micon /><span className='text-dropdown'>Messaging</span></Nav.Link>
-                        <Nav.Link href="#link" className='link-dropdown'><Bicon /><span className='text-dropdown'>Notifications</span></Nav.Link>
+                        <Nav.Link href="#network" className='link-dropdown'><Nicon /><span className='text-dropdown'>My Network</span></Nav.Link>
+                        <Nav.Link href="#jobs" className='link-dropdown'><Jicon /><span className='text-dropdown'>Jobs</span></Nav.Link>
+                        <Nav.Link href="#messaging" className='link-dropdown'><Micon /><span className='text-dropdown'>Messaging</span></Nav.Link>
+                        <Nav.Link href="#notifications" className='link-dropdown'><Bicon /><span className='text-dropdown'>Notifications</span></Nav.Link>
                         <User />
-                        <Nav.Link href="#link" className='link-dropdown'><Wicon /><span className='text-dropdown'>Work</span></Nav.Link>
-                        <Nav.Link href="#link" className='link-dropdown'><span className='text-dropdown'>Try Premium for free</span></Nav.Link>
+                        <Nav.Link href="#profile" className='link-dropdown'><Wicon /><span className='text-dropdown'>Work</span></Nav.Link>
+                        <Nav.Link href="#work" className='link-dropdown prem'><span className='text-dropdown premium'>Try Premium for free</span></Nav.Link>
                     </Nav>
                 </Navbar.Collapse>
+            </div>
         </Navbar>
 
     )
